dffd9a32ff7fef6a129eba7156f089e910c9e258
diablillo99lp <joan.hurtado.garcia@gmail.com>
2022-06-27 18:15:42 +0200

    carpetas conectadas y se muestran

--- src/app/component/index/inicio.component.ts
+++ src/app/component/index/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Carpetas } from 'src/app/models/carpetas';
+import { CarpetasService } from 'src/app/services/carpetas.service';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  carpetas?:Carpetas[];
+
+  constructor(private carpetasService: CarpetasService) { }
 
   ngOnInit(): void {
+    this.getAllCarpetas();
   }
+
+     // Get list
+     getAllCarpetas(): void {
+      this.carpetasService.list()
+        .subscribe(
+          (carpetas: any) => {
+            this.carpetas = carpetas;
+          },
+          (error: any) => {
+            console.log(error);
+          });
+    }
 
 }
---
+++ src/app/models/carpetas.spec.ts
@@ -0,0 +1,7 @@
+import { Carpetas } from './carpetas';
+
+describe('Carpetas', () => {
+  it('should create an instance', () => {
+    expect(new Carpetas()).toBeTruthy();
+  });
+});
