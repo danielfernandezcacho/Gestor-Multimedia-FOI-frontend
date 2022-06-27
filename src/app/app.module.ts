0989b4f617c2c5acf16b16b7148a94155525207e
danielfernandezcacho <dani.fernandezcacho@gmail.com>
2022-06-27 09:07:59 +0200

    cambioos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { RegisterComponent } from './component/register/register.component';
 import { LoginComponent } from './component/login/login.component';
 import { HomeComponent } from './component/home/home.component';
+import { InicioComponent } from './component/index/inicio.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { HomeComponent } from './component/home/home.component';
     RegisterComponent,
     LoginComponent,
     HomeComponent,
+    InicioComponent,
   ],
   imports: [
     BrowserModule,
