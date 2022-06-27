48aea9f407a02aaed64d1bb3b691b637744a63f8
danielfernandezcacho <dani.fernandezcacho@gmail.com>
2022-06-27 09:15:04 +0200

    sd

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {LoginService} from '../../services/auth/login.service'
 
 @Component({
   selector: 'app-login',
@@ -8,39 +7,10 @@ import {LoginService} from '../../services/auth/login.service'
 })
 export class LoginComponent implements OnInit {
 
-  token: any = 'token inicial';
-
-  login: any = {
-    username: '',
-    password: ''
-  };
-  submitted = false;
-
-  constructor(private loginService: LoginService) { }
+  constructor() { }
 
   ngOnInit(): void {
-
   }
 
-  
-  logUser(): void {
-    const data = {
-      username: this.login.username,
-      password: this.login.password,
-    };
 
-    this.loginService.signup(data)
-      .subscribe(
-        (        response: any) => {
-          this.token = response;
-          this.submitted = true;
-          console.log(response);
-          window.sessionStorage.setItem("auth-token", this.token.token);
-          window.sessionStorage.setItem("auth-username", this.login.username);
-        },
-        (        error: any) => {
-          console.log(error);
-        });
-
-  }
 }
