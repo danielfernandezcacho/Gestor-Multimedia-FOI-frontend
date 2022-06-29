ba8b7384e58a7b70d36d346968b100c732216392
danielfernandezcacho <dani.fernandezcacho@gmail.com>
2022-06-29 07:53:55 +0200

    caombios

--- src/app/component/register/register.component.ts
+++ src/app/component/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Usuario } from 'src/app/models/usuario';
+import { UsuarioService } from 'src/app/services/Usuarios/usuarios.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = {
+    nombre:'',
+    contrasenya:'',
+    rol:'USER'
+  }
+
+  constructor(private usuarioservice: UsuarioService) { }
 
   ngOnInit(): void {
   }
 
+  agregarUser():void{
+    const data ={
+      contrasenya:this.usuario.contrasenya,
+      nombre:this.usuario.nombre,
+      rol:this.usuario.rol
+    };
+    this.usuarioservice.create(data).subscribe
+    (
+      response=> {
+        console.log(response);
+      }
+    );
+  }
 }
---
+++ src/app/services/Usuarios/usuarios.service.ts
@@ -0,0 +1,48 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UsuarioService {
+  apiUrl: string = 'https://gestor-multimedia-api.herokuapp.com/usuarios/';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
+
+  constructor(private httpClient: HttpClient) {}
+
+  // Show lists of item
+  list(): Observable<any> {
+    return this.httpClient.get(this.apiUrl).pipe(catchError(this.handleError));
+  }
+
+  create(data: any): Observable<any> {
+    return this.httpClient
+      .post(this.apiUrl, data)
+      .pipe(catchError(this.handleError));
+  }
+
+  // Delete
+  delete(id: any): Observable<any> {
+    return this.httpClient
+      .delete(`${this.apiUrl}/${id}`)
+      .pipe(catchError(this.handleError));
+  }
+
+  // Handle API errors
+  handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(
+        `Backend returned code ${error.status}, ` + `body was: ${error.error}`
+      );
+    }
+    return throwError('Something bad happened; please try again later.');
+  }
+}
